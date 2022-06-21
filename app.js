55fe16ace9b759628e8822d2ed8194fab81ad5b8
jef901 <39604356+jef901@users.noreply.github.com>
2022-06-21 04:00:21 -0300

    Sacamos el texto de nodemon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World! con nodemon')
+  res.status(200).send('<h1>Hello World!</h1>')
 })
 
 app.listen(port, () => {
